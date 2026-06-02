baeb8ff33b7b3ae69eedcc4f6d6cc66834ea3a8c
Blank <wangfj11@foxmail.com>
2026-06-02 17:09:48 +0800

    refactor(builder): 提取 EditorPrefs 读写辅助方法

--- Editor/AssetBundleBuilder/AssetBundleBuilderSetting.cs
+++ Editor/AssetBundleBuilder/AssetBundleBuilderSetting.cs
@@ -6,88 +6,106 @@ namespace YooAsset.Editor
 {
     public static class AssetBundleBuilderSetting
     {
+        private static T GetEnumSetting<T>(string key, T defaultValue) where T : struct
+        {
+            return (T)(object)EditorPrefs.GetInt(key, (int)(object)defaultValue);
+        }
+        private static void SetEnumSetting<T>(string key, T value) where T : struct
+        {
+            EditorPrefs.SetInt(key, (int)(object)value);
+        }
+
+        private static string GetStringSetting(string key, string defaultValue = "")
+        {
+            return EditorPrefs.GetString(key, defaultValue);
+        }
+        private static void SetStringSetting(string key, string value)
+        {
+            EditorPrefs.SetString(key, value);
+        }
+
         // EBuildPipeline
         public static EBuildPipeline GetPackageBuildPipeline(string packageName)
         {
             string key = $"{Application.productName}_{packageName}_{nameof(EBuildPipeline)}";
-            return (EBuildPipeline)EditorPrefs.GetInt(key, (int)EBuildPipeline.BuiltinBuildPipeline);
+            return GetEnumSetting(key, EBuildPipeline.BuiltinBuildPipeline);
         }
         public static void SetPackageBuildPipeline(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{nameof(EBuildPipeline)}";
-            EditorPrefs.SetInt(key, (int)buildPipeline);
+            SetEnumSetting(key, buildPipeline);
         }
 
         // EBuildMode
         public static EBuildMode GetPackageBuildMode(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EBuildMode)}";
-            return (EBuildMode)EditorPrefs.GetInt(key, (int)EBuildMode.ForceRebuild);
+            return GetEnumSetting(key, EBuildMode.ForceRebuild);
         }
         public static void SetPackageBuildMode(string packageName, EBuildPipeline buildPipeline, EBuildMode buildMode)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EBuildMode)}";
-            EditorPrefs.SetInt(key, (int)buildMode);
+            SetEnumSetting(key, buildMode);
         }
 
         // ECompressOption
         public static ECompressOption GetPackageCompressOption(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(ECompressOption)}";
-            return (ECompressOption)EditorPrefs.GetInt(key, (int)ECompressOption.LZ4);
+            return GetEnumSetting(key, ECompressOption.LZ4);
         }
         public static void SetPackageCompressOption(string packageName, EBuildPipeline buildPipeline, ECompressOption compressOption)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(ECompressOption)}";
-            EditorPrefs.SetInt(key, (int)compressOption);
+            SetEnumSetting(key, compressOption);
         }
 
         // EFileNameStyle
         public static EFileNameStyle GetPackageFileNameStyle(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EFileNameStyle)}";
-            return (EFileNameStyle)EditorPrefs.GetInt(key, (int)EFileNameStyle.HashName);
+            return GetEnumSetting(key, EFileNameStyle.HashName);
         }
         public static void SetPackageFileNameStyle(string packageName, EBuildPipeline buildPipeline, EFileNameStyle fileNameStyle)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EFileNameStyle)}";
-            EditorPrefs.SetInt(key, (int)fileNameStyle);
+            SetEnumSetting(key, fileNameStyle);
         }
 
         // EBuildinFileCopyOption
         public static EBuildinFileCopyOption GetPackageBuildinFileCopyOption(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EBuildinFileCopyOption)}";
-            return (EBuildinFileCopyOption)EditorPrefs.GetInt(key, (int)EBuildinFileCopyOption.None);
+            return GetEnumSetting(key, EBuildinFileCopyOption.None);
         }
         public static void SetPackageBuildinFileCopyOption(string packageName, EBuildPipeline buildPipeline, EBuildinFileCopyOption buildinFileCopyOption)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_{nameof(EBuildinFileCopyOption)}";
-            EditorPrefs.SetInt(key, (int)buildinFileCopyOption);
+            SetEnumSetting(key, buildinFileCopyOption);
         }
 
         // BuildFileCopyParams
         public static string GetPackageBuildinFileCopyParams(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_BuildFileCopyParams";
-            return EditorPrefs.GetString(key, string.Empty);
+            return GetStringSetting(key, string.Empty);
         }
         public static void SetPackageBuildinFileCopyParams(string packageName, EBuildPipeline buildPipeline, string buildinFileCopyParams)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_BuildFileCopyParams";
-            EditorPrefs.SetString(key, buildinFileCopyParams);
+            SetStringSetting(key, buildinFileCopyParams);
         }
 
         // EncyptionClassName
         public static string GetPackageEncyptionClassName(string packageName, EBuildPipeline buildPipeline)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_EncyptionClassName";
-            return EditorPrefs.GetString(key, string.Empty);
+            return GetStringSetting(key, string.Empty);
         }
         public static void SetPackageEncyptionClassName(string packageName, EBuildPipeline buildPipeline, string encyptionClassName)
         {
             string key = $"{Application.productName}_{packageName}_{buildPipeline}_EncyptionClassName";
-            EditorPrefs.SetString(key, encyptionClassName);
+            SetStringSetting(key, encyptionClassName);
         }
     }
 }
